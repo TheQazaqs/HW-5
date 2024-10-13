e90a2c9af95db01e618bfbcc4d33c706760c1f39
TheQazaqs <hikoma501@gmail.com>
2024-10-13 18:26:01 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System; 
+
+namespace HW_5
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount bankaccount = new BankAccount("145537258", "Eldos");
+            BankAccount user = new BankAccount("172641756", "Rahat");
+            bankaccount.Deposit(2000);
+            bankaccount.Withdraw(1000);
+            bankaccount.Transfer(user, 1000);
+            Console.WriteLine(user.GetAccountInfo());
+            Console.WriteLine(bankaccount.GetAccountInfo());
+        }
+
+    }
+
+}
